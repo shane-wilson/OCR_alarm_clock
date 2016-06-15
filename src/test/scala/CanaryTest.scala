78c54b2de3decc0d12f0d3179603e97068e70201
Olly Shaw <ollyjshaw@gmail.com>
2016-06-15 20:31:57 +0100

    start with ignore

--- src/test/scala/CanaryTest.scala
+++ src/test/scala/CanaryTest.scala
@@ -2,8 +2,8 @@ import org.scalatest.{Matchers, FlatSpec}
 
 class CanaryTest extends FlatSpec with Matchers{
 
-  "2 + 2 " should "equal 4" in {
-    2 + 2 should be (4)
+  "2 + 2 " should "equal 4" ignore {
+    2 + 2 should be (5)
   }
 
 }
