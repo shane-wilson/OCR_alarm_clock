264101c7d8a2a4ff657279d75ea81f8089849b6c
Olly Shaw <ollyjshaw@gmail.com>
2017-02-18 14:34:03 +0000

    correct test syntax

--- src/test/scala/CanaryTest.scala
+++ src/test/scala/CanaryTest.scala
@@ -2,7 +2,7 @@ import org.scalatest.{Matchers, FlatSpec}
 
 class CanaryTest extends FlatSpec with Matchers{
 
-  "2 + 2 " should "equal 4" {
+  "2 + 2 " should "equal 4" in {
     2 + 2 should be (5)
   }
 
