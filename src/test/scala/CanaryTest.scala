499ac254b5e4e067ec687085bcc9d2cd676222dd
Olly Shaw <ollyjshaw@gmail.com>
2017-02-18 14:32:27 +0000

    failing 1st test

--- src/test/scala/CanaryTest.scala
+++ src/test/scala/CanaryTest.scala
@@ -2,7 +2,7 @@ import org.scalatest.{Matchers, FlatSpec}
 
 class CanaryTest extends FlatSpec with Matchers{
 
-  "2 + 2 " should "equal 4" ignore {
+  "2 + 2 " should "equal 4" {
     2 + 2 should be (5)
   }
 
