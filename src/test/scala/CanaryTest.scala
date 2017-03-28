8131c77147252e5021da02a175561636058d7afa
shane.wilson <shane.wilson@digital.gmrc.gov.uk>
2017-03-28 09:46:31 +0100

    Set base for training

--- src/test/scala/CanaryTest.scala
+++ src/test/scala/CanaryTest.scala
@@ -1,9 +1,9 @@
-import org.scalatest.{Matchers, FlatSpec}
-
-class CanaryTest extends FlatSpec with Matchers{
-
-  "2 + 2 " should "equal 4" in {
-    2 + 2 should be (5)
-  }
-
-}
+//import org.scalatest.{Matchers, FlatSpec}
+//
+//class CanaryTest extends FlatSpec with Matchers{
+//
+//  "2 + 2 " should "equal 4" in {
+//    2 + 2 should be (4)
+//  }
+//
+//}
